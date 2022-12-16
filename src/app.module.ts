7cce63c4d9fe719325d0fe23c0d20a55e57b1a15
Magizchi <g.rajan1993@gmail.com>
2022-12-16 19:38:35 +0100

    Config and add TypeOrm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env'
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mariadb',
+      host: process.env.DATA_BASE_HOST,
+      port: parseInt(process.env.DATA_BASE_PORT),
+      username: process.env.DATA_BASE_USER,
+      password: process.env.DATA_BASE_PASSWORD,
+      database: process.env.DATA_BASE,
+      entities: [join(__dirname, '**', '*.model.{ts,js}')],
+      synchronize: true,
+
+    })],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+
+export class AppModule { }
