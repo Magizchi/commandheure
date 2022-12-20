6a71fb01bcb5043361d205637144c044d17d6456
Magizchi <g.rajan1993@gmail.com>
2022-12-20 18:55:35 +0100

    client<orga:Table>get extends TableProps from antD

--- client/src/components/organims/Table/index.tsx
+++ client/src/components/organims/Table/index.tsx
@@ -1,14 +1,14 @@
 import React, { FunctionComponent } from "react";
-import { Table as TableAnt } from "antd";
+import { Table as TableAnt, TableProps as TableAntProps } from "antd";
 import { ColumnsType } from "antd/es/table";
 
-interface TableProps<TItem extends object = {}> {
+interface TableProps<TItem extends object = {}> extends TableAntProps<TItem> {
   columns: ColumnsType<TItem>;
   data: TItem[];
 }
 
-const Table = <TItem extends object>({ columns, data }: TableProps<TItem>) => (
-  <TableAnt columns={columns} dataSource={data} />
+const Table = <TItem extends object>({ columns, data, ...props }: TableProps<TItem>) => (
+  <TableAnt columns={columns} dataSource={data} {...props} />
 );
 
 export default Table;
