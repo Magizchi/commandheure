1f152a2073f7bfb1fc4b852338e09233fe22fb73
Magizchi <g.rajan1993@gmail.com>
2022-12-20 18:25:00 +0100

    client<Model:Products>created

---
+++ client/src/pages/products/models/product.models.ts
@@ -0,0 +1,12 @@
+export interface Products {
+    id: number;
+    category_id: number;
+    name: string;
+    brand: string;
+    quantity_per_box: string;
+    ean: string;
+    code_supplier: string;
+    image_path: string;
+    douane: string;
+    weight: string;
+}
