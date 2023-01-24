3d969e7fa7a4836d3e4130318f2dd70ea97600fd
Magizchi <g.rajan1993@gmail.com>
2023-01-24 16:46:27 +0100

    api:module<products>add httpModule

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -6,9 +6,10 @@ import { Product } from './entities/product.entity';
 import { CategoriesService } from 'src/categories/categories.service';
 import { Category } from 'src/categories/entities/category.entity';
 import { CategoriesController } from 'src/categories/categories.controller';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Product, Category])],
+  imports: [TypeOrmModule.forFeature([Product, Category]), HttpModule],
   controllers: [ProductsController, CategoriesController],
   providers: [ProductsService, CategoriesService]
 })
