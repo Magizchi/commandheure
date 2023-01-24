a2e94dd10977881a3d7d1df546a7d1c7f27b75f6
Magizchi <g.rajan1993@gmail.com>
2023-01-24 16:45:38 +0100

    api:service<products>upload file csv + get image from openfoodfacts

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,25 +1,23 @@
+import { HttpService } from '@nestjs/axios';
 import { Inject, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { catchError, firstValueFrom } from 'rxjs';
 import { CategoriesService } from 'src/categories/categories.service';
-import { Category } from 'src/categories/entities/category.entity';
-import { Not, Repository } from 'typeorm';
+import { Like, Repository } from 'typeorm';
 import { CreateProductDto } from './dto/create-product.dto';
-import { UpdateProductDto } from './dto/update-product.dto';
 import { Product } from './entities/product.entity';
+
 
 @Injectable()
 export class ProductsService {
+  logger: any;
   constructor(
     @InjectRepository(Product)
     private readonly productRepository: Repository<Product>,
     @Inject(CategoriesService)
-    private readonly categoriesService: CategoriesService
+    private readonly categoriesService: CategoriesService,
+    private readonly httpService: HttpService
   ) { }
-
-
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
-  }
 
   async upload(file: string) {
     const csvInfoArray = file.split('\r\n').map(each => each.split(','))
@@ -52,33 +50,117 @@ export class ProductsService {
 
     return 'Upload file'
   }
-
-  async findByCategory(id: string) {
-    const categoryName = await this.categoriesService.findOneBy(id.replace('-', ' ').toUpperCase())
-    const productPerCategory = await this.productRepository.find({
+  /**
+   * @param category category of products
+   * @param page asked pages
+   * @param take take products
+   * @returns 
+   */
+  async findByCategory(category: string, page: number, take: number) {
+    const categoryName = await this.categoriesService.findOneBy(category.replace('-', ' ').toUpperCase())
+    const [productPerCategory, filtered] = await this.productRepository.findAndCount({
       where: {
         category_id: categoryName.id
+      },
+      skip: (page - 1) * take,
+      take: take,
+      order: {
+        code_supplier: 'asc'
       }
     })
-    console.log('products_aa', productPerCategory.length);
+    console.log('productPerCategory', productPerCategory);
 
-    return productPerCategory
+    const result = {
+      products: productPerCategory.sort((a, b) => +a.code_supplier - +b.code_supplier).map((item, index) => ({ ...item, key: 1 + index + (page - 1) * take })),
+      totals: {
+        filterd: filtered,
+        total: filtered
+      }
+    }
+    return result
+  }
+
+  async searchProducts(search: string) {
+    const filteredProduct = await this.productRepository.findBy([
+      { name: Like(`%${search}%`) },
+      { code_supplier: Like(`%${search}%`) }
+    ])
+    return filteredProduct.map((product) => ({ ...product, label: product.name, value: product.code_supplier }))
+  }
+
+  async positionProduct(id: string) {
+    const product = await this.productRepository.find(
+      {
+        where: {
+          code_supplier: id
+        },
+      }
+    )
+    const p = product[0]
+
+    const test = await this.searchProducts(p.name)
 
   }
 
-  findAll() {
-    return `This action returns all products`;
-  }
+  async getImages() {
+    const allProducts = await this.productRepository.find()
+    await Promise.all(allProducts.map(async (product) => {
+      // console.log('pr', product.code_supplier)
+      const response: any = await firstValueFrom(
+        this.httpService.get<any>(`https://world.openfoodfacts.org/api/v3/product/${product.ean}`).pipe(
+          catchError((error: any) => {
+            return this.logger.error(error.response.data);
+          })
+        ))
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
-  }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
-  }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+      const [findProduct] = await this.productRepository.find(
+        {
+          where: {
+            code_supplier: product.code_supplier
+          },
+        }
+      )
+
+      // console.log('findProduct', findProduct);
+
+      const addedImage = { ...findProduct, image: response.data.product.image_front_small_url }
+      // console.log('addedImage', addedImage)
+      await this.productRepository.save(addedImage)
+
+      // if (response) {  
+
+      //   response.forEach((e: any) => {
+      //     if (e) {
+      //       console.log('res', e)
+      //     }
+      //   });
+      // }
+
+
+    }))
+
+
   }
 }
+
+// create(createProductDto: CreateProductDto) {
+//   return 'This action adds a new product';
+// }
+
+// findAll() {
+//   return `This action returns all products`;
+// }
+
+// findOne(id: number) {
+//   return `This action returns a #${id} product`;
+// }
+
+// update(id: number, updateProductDto: UpdateProductDto) {
+//   return `This action updates a #${id} product`;
+// }
+
+// remove(id: number) {
+//   return `This action removes a #${id} product`;
+// }
