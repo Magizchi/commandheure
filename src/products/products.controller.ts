64099204d51465b310bb2277991ff11aebe7a5bc
Magizchi <g.rajan1993@gmail.com>
2023-02-09 17:23:40 +0100

    api:refacto<controllers:products>delete unsused code and rename func

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,18 +1,10 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseInterceptors, UploadedFile, Query, Redirect } from '@nestjs/common';
+import { Controller, Get, Post, Param, UseInterceptors, UploadedFile, Query } from '@nestjs/common';
 import { ProductsService } from './products.service';
-import { CreateProductDto } from './dto/create-product.dto';
-import { UpdateProductDto } from './dto/update-product.dto';
 import { FileInterceptor } from '@nestjs/platform-express';
-import { CategoriesService } from 'src/categories/categories.service';
 
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) { }
-
-  // @Post()
-  // create(@Body() createProductDto: CreateProductDto) {
-  //   return this.productsService.create(createProductDto);
-  // }
 
   @Post('upload')
   @UseInterceptors(FileInterceptor('file'))
@@ -23,16 +15,14 @@ export class ProductsController {
   }
 
   @Get()
-  async searchProduct(@Query("search") search): Promise<any> {
+  async searchProducts(@Query("search") search): Promise<any> {
     const searchProducts = await this.productsService.searchProducts(search)
     return searchProducts
   }
 
   @Get(':id')
-  findByCategory(@Param('id') id: string, @Query() search) {
-    console.log('search', search);
-
-    return this.productsService.findByCategory(id, search.page, search.size)
+  getProducts(@Param('id') id: string, @Query() search: { page: number, size: number }) {
+    return this.productsService.getProducts(id, search.page, search.size)
   }
 
   @Get('site/:id')
@@ -47,24 +37,4 @@ export class ProductsController {
   getImages() {
     return this.productsService.getImages()
   }
-
-  // @Get()
-  // findAll() {
-  //   return this.productsService.findAll();
-  // }
-
-  // @Get(':id')
-  // findOne(@Param('id') id: string) {
-  //   return this.productsService.findOne(+id);
-  // }
-
-  // @Patch(':id')
-  // update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
-  //   return this.productsService.update(+id, updateProductDto);
-  // }
-
-  // @Delete(':id')
-  // remove(@Param('id') id: string) {
-  //   return this.productsService.remove(+id);
-  // }
 }
