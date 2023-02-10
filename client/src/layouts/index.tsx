b26433021bbac6e39e9013f94b80751ab51f9598
Magizchi <g.rajan1993@gmail.com>
2023-02-10 15:48:22 +0100

    client:feat<layout>create

---
+++ client/src/layouts/index.tsx
@@ -0,0 +1,43 @@
+import axios from "axios";
+import classNames from "classnames";
+import React, { FunctionComponent, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+import { API } from "../constants/API";
+
+const Layout: FunctionComponent<{ children: React.ReactNode }> = ({ children }) => {
+  const [menu, setmenu] = useState<string[]>([]);
+  const location = useLocation();
+
+  const getMenu = async () => {
+    const response = await axios.get(API.MENU);
+    setmenu(response.data);
+  };
+
+  React.useEffect(() => {
+    getMenu();
+  }, []);
+
+  return (
+    <div className="container flex justify-between mx-auto">
+      <aside className="sticky flex flex-col h-full mt-20 mr-5 top-20 w-36">
+        <ul className="flex flex-col bg-white border rounded-md shadow-sm border-primary text-primary">
+          {menu.map((data: string, index: number) => (
+            <Link
+              key={index}
+              to={API.PRODUCTS + `/${data.toLowerCase()}?size=20&page=1`}
+              className={classNames("px-2 py-1 hover:bg-primary hover:text-white", {
+                "bg-primary text-white": location.pathname.includes(data.toLowerCase()),
+                "bg-none text-primary": !location.pathname.includes(data.toLowerCase()),
+              })}
+            >
+              {data}
+            </Link>
+          ))}
+        </ul>
+      </aside>
+      <main className="mt-6 w-full">{children}</main>
+    </div>
+  );
+};
+
+export default Layout;
