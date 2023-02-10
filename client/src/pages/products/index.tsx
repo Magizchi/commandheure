c9dd11443106bb26e5b07eb79a28bd775df570fe
Magizchi <g.rajan1993@gmail.com>
2023-02-10 13:33:01 +0100

    client:feat<page:product>improve creat/update shoppingcart

--- client/src/pages/products/index.tsx
+++ client/src/pages/products/index.tsx
@@ -1,64 +1,109 @@
-import { ColumnsType } from "antd/es/table";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import Table from "../../components/organims/Table";
+import { useParams, useSearchParams } from "react-router-dom";
+import Card from "../../components/atoms/Card";
 import { API } from "../../constants/API";
-import { Products } from "./models/product.models";
+import { FormatProduct, Product } from "./models/product.models";
+import { InputNumber } from "antd";
+import Button from "../../components/atoms/Button";
+import Pagination from "../../components/molecules/pagination";
+import useArrayHook from "../../hooks/useArray";
+import useDelay from "../../hooks/useHookDelay";
 
 const ProductsPage = () => {
-  const [products, setProducts] = useState<Products[]>([]);
+  const { array: products, save, update } = useArrayHook<Product>([]);
+  const [totalProducts, setTotalProducts] = useState<number>(0);
+  const [searchParams] = useSearchParams();
   const params = useParams();
 
   const getProducts = async () => {
-    const response = await axios.get(API.PRODUCTS_PER_CATEGORY + params.id);
-    setProducts(response.data);
+    const {
+      data: { products, totals },
+    } = await axios.get(API.PRODUCTS + `/${params.id}?${searchParams.toString()}`);
+
+    save(FormatProduct(products));
+    setTotalProducts(totals);
   };
 
   useEffect(() => {
     getProducts();
-  }, [params.id]);
+  }, [params.id, searchParams]);
+
+  const onChange = async (value: number, product: Product) => {
+    try {
+      if (value < 0) value = 0;
+      const modulo = value % product.quantity_per_box;
+      const diff = product.quantity_per_box - modulo;
+      if (modulo < 5) {
+        value = value - modulo;
+      } else {
+        value = value + diff;
+      }
+      const { id, ...body } = {
+        id: product.shoppingCart.id,
+        product: product.id,
+        quantities: value,
+      };
+
+      if (id) {
+        await axios.patch(API.SHOPPING_CART + `/${id}`, body);
+      } else {
+        await axios.post(API.SHOPPING_CART, body);
+      }
+
+      update({ ...product, shoppingCart: { id: product.shoppingCart?.id ?? null, quantities: value } });
+    } catch (err) {
+      throw `Erreur:${err}`;
+    }
+  };
 
   return (
-    <section className="">
-      <Table
-        columns={columns}
-        data={products}
-        size="small"
-        pagination={{
-          defaultPageSize: 20,
-          position: ["topRight", "bottomRight"],
-        }}
-      />
+    <section className="flex items-center flex-col">
+      <Pagination defaultCurrent={1} total={totalProducts} />
+      <div className="flex flex-wrap">
+        {products.map((product) => (
+          <Card
+            hoverable
+            key={product.code_supplier}
+            className="w-64 m-2"
+            cover={
+              <img className="flex justify-center items-center h-60 bg-contain" alt="exemple" src={product.image} />
+            }
+          >
+            <div className="flex flex-col justify-between h-40">
+              <div>
+                <p className="m-0 p-0 ">{product.name}</p>
+                <p>Colisage: {product.quantity_per_box}</p>
+              </div>
+              <div className="flex justify-center">
+                <Button
+                  className="mr-1 flex justify-center items-center p-2"
+                  onClick={() => onChange(+product.shoppingCart.quantities - +product.quantity_per_box, product)}
+                >
+                  -
+                </Button>
+                <InputNumber
+                  keyboard={false}
+                  controls={false}
+                  value={product.shoppingCart.quantities}
+                  className=""
+                  min={0}
+                  onChange={useDelay((value) => onChange(value ?? 0, product), 500)}
+                />
+                <Button
+                  className="ml-1 flex justify-center items-center p-2"
+                  onClick={() => onChange(+product.shoppingCart.quantities + +product.quantity_per_box, product)}
+                >
+                  +
+                </Button>
+              </div>
+            </div>
+          </Card>
+        ))}
+      </div>
+      <Pagination defaultCurrent={1} total={totalProducts} />
     </section>
   );
 };
-
-const columns: ColumnsType<Products> = [
-  {
-    title: "code Fournisseur",
-    dataIndex: "code_supplier",
-    key: "code_supplier",
-    width: "10em",
-  },
-  {
-    title: "Nom du produit",
-    dataIndex: "name",
-    key: "name",
-    width: "auto",
-  },
-  {
-    title: "PCB",
-    dataIndex: "quantity_per_box",
-    key: "quantity_per_box",
-    width: "8em",
-  },
-  {
-    title: "Marque",
-    dataIndex: "brand",
-    key: "brand",
-    width: "8em",
-  },
-];
 
 export default ProductsPage;
