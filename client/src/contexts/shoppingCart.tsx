037140d8a2e7f40ec3a51a0a52ec7b75463b7875
Magizchi <g.rajan1993@gmail.com>
2023-02-12 17:39:11 +0100

    client:feat<context:ShoppingCart>create context to change produt in shoppingcart list

---
+++ client/src/contexts/shoppingCart.tsx
@@ -0,0 +1,30 @@
+import React, { FunctionComponent, useContext, useState } from "react";
+import { Product } from "../pages/products/models/product.models";
+
+interface ProductListContextProps {
+  product: Product;
+  setProduct: React.Dispatch<React.SetStateAction<Product>>;
+}
+
+const ProductListContext = React.createContext<ProductListContextProps>({
+  product: {} as Product,
+  setProduct: () => {},
+});
+
+const ShoppingCartProvider: FunctionComponent<{ children: React.ReactNode }> = ({ children }) => {
+  const [product, setProduct] = useState<Product>({} as Product);
+
+  return (
+    <ProductListContext.Provider value={{ product: product, setProduct: setProduct }}>
+      {children}
+    </ProductListContext.Provider>
+  );
+};
+
+const useShoppingCartContext = (): ProductListContextProps => {
+  const context = useContext(ProductListContext);
+  if (context === undefined) throw new Error("useProductListContext must be used within a ShoppingCartProvider");
+  return context as ProductListContextProps;
+};
+
+export { ShoppingCartProvider, useShoppingCartContext };
