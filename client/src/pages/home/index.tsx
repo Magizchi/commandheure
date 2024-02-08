d9a76847362cb1b45743600b082549c646d67205
Rajanan <g.rajan1993@gmail.com>
2024-02-08 17:57:07 +0000

    client(home:page):created

---
+++ client/src/pages/home/index.tsx
@@ -0,0 +1,12 @@
+import React, { useEffect } from 'react';
+import { redirectDocument } from "react-router-dom";
+
+const HomePage = () => {
+    return (
+        <section>
+            <h1>Home Page</h1>
+        </section>
+    )
+}
+
+export default HomePage
