97e95d75a4c25ff1561039a0ed0aa0c73487662a
Rajanan <g.rajan1993@gmail.com>
2024-02-10 17:41:44 +0000

    client(Organism:Table):delete all paginiation method

--- client/src/components/organims/Table/index.tsx
+++ client/src/components/organims/Table/index.tsx
@@ -1,48 +1,16 @@
-import React, { useEffect, useState } from "react";
 import { Table as TableAnt, TableProps as TableAntProps } from "antd";
-import { ColumnsType, TablePaginationConfig } from "antd/es/table";
-import { useSearchParams } from "react-router-dom";
 
 interface TableProps<TItem extends object = {}> extends TableAntProps<TItem> {
-  columns: ColumnsType<TItem>;
+  columns: any;
   data: TItem[];
-  total: number;
 }
 
-const Table = <TItem extends object>({ columns, data, total, ...props }: TableProps<TItem>) => {
-  const [searchParams, setParams] = useSearchParams();
-  const [pagination, setPagination] = useState({
-    page: +searchParams.get("page")! ?? 1,
-    defaultPageSize: +searchParams.get("size")! ?? 20,
-    position: ["topRight", "bottomRight"],
-  });
-
-  useEffect(() => {
-    setPagination((curr) => ({
-      ...curr,
-      page: +searchParams.get("page")!,
-      defaultPageSize: +searchParams.get("size")!,
-    }));
-  }, [searchParams]);
-
-  const onChange = (pagination: TablePaginationConfig) => {
-    setPagination((curr) => ({ ...curr, page: +pagination.current!, defaultPageSize: pagination.defaultPageSize! }));
-    setParams(`size=${pagination.pageSize}&page=${pagination.current}`);
-  };
-
+const Table = <TItem extends object>({ columns, data, ...props }: TableProps<TItem>) => {
   return (
     <TableAnt
       columns={columns}
       dataSource={data}
-      onChange={onChange}
-      pagination={{
-        defaultPageSize: pagination.defaultPageSize,
-        pageSize: pagination.defaultPageSize,
-        total: total,
-        defaultCurrent: pagination.page,
-        current: pagination.page,
-        position: ["topRight", "bottomRight"],
-      }}
+      pagination={false}
       {...props}
     />
   );
