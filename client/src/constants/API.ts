58a258299f6558405c500a9c56be64ad1cdbbf6b
Rajanan <g.rajan1993@gmail.com>
2024-02-15 13:52:31 +0000

    clien(constant:API):add route for excel/csv

--- client/src/constants/API.ts
+++ client/src/constants/API.ts
@@ -1,5 +1,6 @@
 export enum API {
     MENU = "/api/categories/menu",
     PRODUCTS = "/api/products",
-    SHOPPING_CART = "/api/shopping-cart"
+    SHOPPING_CART = "/api/shopping-cart",
+    SHOPPING_CART_CSV = SHOPPING_CART + '/excel'
 }
