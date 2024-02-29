6dae6a4e4331ef53b05065ab95c159044fa6a54a
Rajanan <g.rajan1993@gmail.com>
2024-02-29 12:37:42 +0000

    server(package):add class-validator

--- apps/api/.eslintrc.js
+++ apps/api/.eslintrc.js
@@ -7,6 +7,7 @@ module.exports = {
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
+    'prettier',
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
   ],
