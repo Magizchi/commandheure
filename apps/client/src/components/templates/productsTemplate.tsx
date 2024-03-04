c5fabc7d98eac0dd914cb47050f951295ed0cd36
Magizchi <g.rajan1993@gmail.com>
2024-03-04 12:04:25 +0000

    client:style(template:product):add loading spin

--- apps/client/src/components/templates/productsTemplate.tsx
+++ apps/client/src/components/templates/productsTemplate.tsx
@@ -6,7 +6,7 @@ import { API } from '@constants/API';
 import { useParams } from 'react-router-dom';
 import useArrayHook from '@hooks/useArray';
 import ShowProducs from '@components/organims/ShowProducts';
-import { TableProps } from 'antd';
+import { Spin, TableProps } from 'antd';
 import Table from '@components/organims/Table';
 import { Product, ProductVariant } from '@pages/products/models/product.models';
 import Link from '@components/atoms/Link';
@@ -16,6 +16,7 @@ const ProductsTemplate = () => {
     const [menu, setmenu] = useState<string[]>([]);
     const { array: products, save: saveProducts } = useArrayHook<Product>([]);
     const { array: shoppingCart, save: saveShoppingCart, show } = useArrayHook<ProductVariant>([]);
+    const [loading, setLoading] = useState<boolean>(true);
     const params = useParams();
 
     const getProducts = async () => {
@@ -44,8 +45,12 @@ const ProductsTemplate = () => {
         if (menu.length === 0) getMenu();
         if (shoppingCart.length === 0) getShoppingCart();
         getProducts();
+        setLoading(false);
     }, [params]);
 
+    if (loading) {
+        return <div className="fixed top-[50%] right-[50%]"><Spin size='large' /></div>;
+    }
     return (
         <section className="container flex justify-between mx-auto space-x-5">
             <AsideMenu menu={menu} to={ROUTES.HEADECOEUR_PRODUCTS} />
@@ -53,7 +58,7 @@ const ProductsTemplate = () => {
                 <h2 className="text-2xl bg-gray-100 font-bold uppercase text-primary">{params.id?.toUpperCase()}</h2>
                 {products.map((product, index) => <ShowProducs onChange={setInShoppingCart} {...product} index={index} register={undefined} />)}
             </div>
-            <div className="sticky top-20 h-full flex flex-col flex-grow mt-6 gap-5">
+            <div className="sticky top-20 h-full flex flex-col flex-grow mt-6 gap-5 space-y-3">
                 <h2 className="sticky top-20 z-50 text-2xl uppercase text-primary font-bold">panier</h2>
                 <Table className="w-full" columns={columns} data={show(10)} />
                 <Link href={ROUTES.SHOPPING_CART} className='min-w-24 max-w-48 border-2 border-primary bg-primary text-white text-center p-auto rounded-md hover:bg-stars-500'>Visualiser panier</Link>
