33ed7b3fdd08b5460dd7855759c119440674e2c5
Magizchi <g.rajan1993@gmail.com>
2024-03-04 09:47:00 +0000

    client:config(esLint): add prettier

--- apps/client/.eslintrc.js
+++ apps/client/.eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
         browser: true,
         es2021: true,
     },
-    extends: ["eslint:recommended", "plugin:react/recommended", "plugin:@typescript-eslint/recommended"],
+    extends: ["prettier","eslint:recommended", "plugin:react/recommended", "plugin:@typescript-eslint/recommended"],
     parser: "@typescript-eslint/parser",
     parserOptions: {
         ecmaFeatures: {
