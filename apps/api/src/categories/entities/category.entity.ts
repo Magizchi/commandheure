5db337d63d8661d1b1f7374633193bbffe08a745
Magizchi <g.rajan1993@gmail.com>
2024-03-12 17:34:09 +0000

    api:feat(entities:categories):add relation to company entity

--- apps/api/src/categories/entities/category.entity.ts
+++ apps/api/src/categories/entities/category.entity.ts
@@ -1,5 +1,6 @@
+import { Company } from "src/companies/entities/company.entity";
 import { Product } from "src/products/entities/product.entity";
-import { BaseEntity, Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { BaseEntity, Column, Entity, JoinColumn, ManyToOne, OneToMany, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
 export class Category extends BaseEntity {
@@ -9,6 +10,13 @@ export class Category extends BaseEntity {
     @Column()
     name: string;
 
+    @ManyToOne(() => Company, company => company.category)
+    @JoinColumn({ name: "company_id" })
+    company: number;
+
+    @Column()
+    company_id: number;
+
     @OneToMany(() => Product, (product) => product.category_id)
-    products: Product[]
+    products: Product[];
 }
