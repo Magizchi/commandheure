589ebf9dccf94a0c4407efd4d2c0ecf8df5d9eb1
Magizchi <g.rajan1993@gmail.com>
2024-03-13 19:09:19 +0000

    client:feat(organism:dropdown):created

---
+++ apps/client/src/components/molecules/Dropdown/index.tsx
@@ -0,0 +1,36 @@
+import classNames from 'classnames';
+import { FunctionComponent } from 'react';
+import { Link, Location } from 'react-router-dom';
+
+interface DropdownProps {
+    title: string;
+    menu: readonly string[];
+    location: Location;
+}
+const Dropdown: FunctionComponent<DropdownProps> = ({ title, menu, location }) => {
+    return (
+        <li className='space-y-2'>
+            <p className={classNames("w-full", {
+                "transform after:divide-gray-300 after:border-2 after:absolute after:-bottom-1 after:left-0 after:rounded-s-xl after:w-full ": location.pathname.includes(title.toLocaleLowerCase())
+            })}>
+                {title}
+            </p>
+            <ul className="flex flex-col space-y-1 ml-5 w-full">
+                {
+                    menu.map((item, index) =>
+                        <Link
+                            key={index}
+                            to={`/${title.toLowerCase()}/products/${item.toLowerCase()}`}
+                            className={classNames("hover:bg-gray-300 hover:rounded-s-xl hover:text-primary p-1 w-full", {
+                                "bg-gray-100 text-primary flex flex-row !rounded-s-xl": location.pathname.includes(item.toLowerCase()),
+                                "bg-none text-white": !location.pathname.includes(item.toLowerCase()),
+                            })}>
+                            {item}
+                        </Link>)
+                }
+            </ul>
+        </li >
+    );
+};
+
+export default Dropdown; 
