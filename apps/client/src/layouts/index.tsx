27b48fff937d9021d67ad21bc999dbcbb9894eb6
Magizchi <g.rajan1993@gmail.com>
2024-03-14 10:05:28 +0000

    same

--- apps/client/src/layouts/index.tsx
+++ apps/client/src/layouts/index.tsx
@@ -1,13 +1,12 @@
 import axios from "axios";
-import classNames from "classnames";
 import React, { FunctionComponent, useState } from "react";
-import { Link, useLocation } from "react-router-dom";
 import { API } from "@constants/API";
 import { ROUTES } from "@constants/Routes";
+import Header from "./Header";
+import AsideMenu from "@components/organims/AsideMenu";
 
-const Layout: FunctionComponent<{ children: React.ReactNode }> = ({ children }) => {
+const MainLayout: FunctionComponent<{ children: React.ReactNode; }> = ({ children }) => {
   const [menu, setmenu] = useState<string[]>([]);
-  const location = useLocation();
 
   const getMenu = async () => {
     const response = await axios.get(API.MENU);
@@ -19,26 +18,16 @@ const Layout: FunctionComponent<{ children: React.ReactNode }> = ({ children }) 
   }, []);
 
   return (
-    <div className="container flex justify-between mx-auto">
-      <aside className="sticky flex flex-col h-full mt-20 mr-5 top-20 w-36">
-        <ul className="flex flex-col bg-white border rounded-md shadow-sm border-primary text-primary">
-          {menu.map((data: string, index: number) => (
-            <Link
-              key={index}
-              to={ROUTES.HEADECOEUR_PRODUCTS + `/${data.toLowerCase()}?size=20&page=1`}
-              className={classNames("px-2 py-1 hover:bg-primary hover:text-white", {
-                "bg-primary text-white": location.pathname.includes(data.toLowerCase()),
-                "bg-none text-primary": !location.pathname.includes(data.toLowerCase()),
-              })}
-            >
-              {data}
-            </Link>
-          ))}
-        </ul>
-      </aside>
-      <main className="mt-6 w-full">{children}</main>
-    </div>
+    <>
+      <Header />
+      <main className="container flex justify-between mx-auto">
+        <aside className="sticky flex flex-col h-full mt-20 mr-5 top-20 w-36">
+          <AsideMenu menu={menu} to={ROUTES.HEADECOEUR_PRODUCTS} />
+        </aside>
+        <div className="mt-6 w-full">{children}</div>
+      </main>
+    </>
   );
 };
 
-export default Layout;
+export default MainLayout;
