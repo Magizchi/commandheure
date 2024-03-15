04df581fb3734574416d455a34bf3f87af775c79
Magizchi <g.rajan1993@gmail.com>
2024-03-15 14:01:58 +0000

    client:fix(page:landing):fix path for main button

--- apps/client/src/pages/landing/index.tsx
+++ apps/client/src/pages/landing/index.tsx
@@ -25,7 +25,7 @@ const LandingPage = () => {
             <main className="text-primary font-sans">
                 <section style={{ backgroundImage: "url(/pattern2.svg)" }} className="flex justify-center items-center h-screen flex-col space-y-5 bg-no-repeat bg-cover">
                     <h2 className="text-5xl font-serif text-primary text-center w-1/2">{LandingPageInfos.title}</h2>
-                    <Link to={ROUTES.HEADECOEUR_PRODUCTS} className="bg-main font-bold p-5 text-white rounded-2xl transform hover:scale-105 hover:duration-300 hover:ease-in-out ">ESSAYER LE COMMAND'HEURE</Link>
+                    <Link to={ROUTES.HEADECOEUR_PRODUCTS + '/alwadi'} className="bg-main font-bold p-5 text-white rounded-2xl transform hover:scale-105 hover:duration-300 hover:ease-in-out ">ESSAYER LE COMMAND'HEURE</Link>
                 </section>
 
                 <section style={{ backgroundImage: "url(/benner.jpg)" }} className="flex flex-row space-x-48 justify-center items-center h-screen bg-cover">
