50d9fd176a191c96dcadc1cd10d3d258e198ff91
Magizchi <g.rajan1993@gmail.com>
2024-03-21 13:40:19 +0000

    fix import

--- apps/client/src/components/molecules/MySelect/index.tsx
+++ apps/client/src/components/molecules/MySelect/index.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent } from "react";
+import { FunctionComponent } from "react";
 import AsyncSelect from 'react-select/async';
 import type { AsyncProps } from 'react-select/async';
 
